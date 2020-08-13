e11e84150a3e520a190c1b6ab6932fe8171364a1
spencer <devops>
2020-08-13 17:42:39 -0500

    first commit

---
+++ src/main/java/mx/com/profuturo/demoms/DemoMsApplication.java
@@ -0,0 +1,13 @@
+package mx.com.profuturo.demoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/profuturo/demoMS/DemoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.profuturo.demoMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
