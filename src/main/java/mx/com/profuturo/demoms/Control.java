99ca5de3ffa5dca22ce4d600a57d6defe484b386
spencer <devops>
2020-08-18 17:05:09 -0500

    bd

--- src/main/java/mx/com/profuturo/demoms/Control.java
+++ src/main/java/mx/com/profuturo/demoms/Control.java
@@ -2,23 +2,32 @@ package mx.com.profuturo.demoms;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Control {
 
-	
-	
 	private static final String TEMPLATE = "Hello, %s y la propiedad demo es %s !";
-	
+
 	private final AtomicLong counter = new AtomicLong();
-	
+
 	@Value("${propiedad.demo}")
 	private String ejemploPropiedad;
+
+	@Autowired
+	private UserService userService;
+
+	@PostMapping(value = "/saveUser")
+	public Boolean saveUser(@RequestBody User u) {
+		return userService.saveUser(u);
+	}
 
 	@GetMapping("/hello-world")
 	@ResponseBody
