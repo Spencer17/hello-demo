a20a7bd113098a047248cf6e25b93ef3702d7bac
spencer <devops>
2020-08-18 17:44:49 -0500

    nobd

--- src/main/java/mx/com/profuturo/demoms/Control.java
+++ src/main/java/mx/com/profuturo/demoms/Control.java
@@ -4,22 +4,23 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Control {
 
-	private static final String TEMPLATE = "Hello, %s y la propiedad demo es %s !";
-
+	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@Value("${propiedad.demo}")
 	private String ejemploPropiedad;
 
+	@GetMapping("/hello-world")
 	@ResponseBody
 	public Greeting sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name, ejemploPropiedad));
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
 }
