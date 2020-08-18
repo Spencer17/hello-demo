05e57cd70b94d04b4c6692ded0996a37a89db31b
spencer <devops>
2020-08-18 17:35:54 -0500

    nobd

--- src/main/java/mx/com/profuturo/demoms/Control.java
+++ src/main/java/mx/com/profuturo/demoms/Control.java
@@ -2,12 +2,8 @@ package mx.com.profuturo.demoms;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,15 +17,6 @@ public class Control {
 	@Value("${propiedad.demo}")
 	private String ejemploPropiedad;
 
-	@Autowired
-	private UserService userService;
-
-	@PostMapping(value = "/saveUser")
-	public Boolean saveUser(@RequestBody User u) {
-		return userService.saveUser(u);
-	}
-
-	@GetMapping("/hello-world")
 	@ResponseBody
 	public Greeting sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name, ejemploPropiedad));
--- src/main/java/mx/com/profuturo/demoms/UserService.java
+++
@@ -1,5 +0,0 @@
-package mx.com.profuturo.demoms;
-
-public interface UserService {
-    public Boolean saveUser(User user);
-}
